2a8769642b25f8c43afb34baee06a373800d569c
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-03-29 19:17:50 +0200

    Increase past_words to 10

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ tf.flags.DEFINE_string("data_file_path", "data/corpus.small", "Path to the train
 # Model parameters
 tf.flags.DEFINE_integer("embedding_dim", 50, "Dimensionality of word embeddings (default: 128)")
 tf.flags.DEFINE_integer("vocab_size", 20000, "Size of the vocabulary (default: 50k)")
-tf.flags.DEFINE_integer("past_words", 3, "How many previous words are used for prediction (default: 3)")
+tf.flags.DEFINE_integer("past_words", 10, "How many previous words are used for prediction (default: 3)")
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
 tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
