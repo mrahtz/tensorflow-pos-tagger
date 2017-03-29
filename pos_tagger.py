ad89b6366131794274a2769cb9485f424745966c
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-03-29 19:58:32 +0200

    Revert "Remove target word from feature list"
    This reverts commit 1cee41f8f4b0f0018c944843a74adf9a14ff4e47.

--- pos_tagger.py
+++ pos_tagger.py
@@ -36,21 +36,17 @@ class PoSTagger(object):
 
                 # Create feature vector
 
-                self.input_x_past_words = self.input_x[:, 1:]
-
-                self.word_matrix = tf.nn.embedding_lookup(
-                    self.embedding_matrix,
-                    self.input_x_past_words
-                )
+                self.word_matrix = \
+                    tf.nn.embedding_lookup(self.embedding_matrix, self.input_x)
                 # stack the rows
                 # -1: account for variable batch size
                 # TODO: understand
-                new_shape = [-1, (n_past_words) * embedding_size]
+                new_shape = [-1, (n_past_words + 1) * embedding_size]
                 self.feature_vector = tf.reshape(self.word_matrix, new_shape)
 
                 # send feature vector through hidden layers
 
-                feature_vector_size = (n_past_words) * embedding_size
+                feature_vector_size = (n_past_words + 1) * embedding_size
                 h1_size = 100
                 w1 = tf.Variable(
                     tf.truncated_normal(
