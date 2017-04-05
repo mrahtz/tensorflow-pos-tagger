15d7a9b62ce284755e31b8c616f25657295d6376
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-04-05 19:49:45 +0200

    Minor tidy-ups

--- demo.py
+++ demo.py
@@ -15,21 +15,18 @@ words = sentence.strip().split(" ")
 textloader = TextLoader()
 features = textloader.parse(words)
 
-## EVALUATION ##
-
 session_conf = tf.ConfigProto(allow_soft_placement=True)
 sess = tf.Session(config=session_conf)
 
-# Load the saved meta graph and restore variables
 checkpoint_file = tf.train.latest_checkpoint('runs/1491411201/checkpoints/')
-saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+saver = tf.train.import_meta_graph(checkpoint_file + '.meta')
 saver.restore(sess, checkpoint_file)
 
 graph = tf.get_default_graph()
 input_x = graph.get_operation_by_name("input_x").outputs[0]
 predictions = graph.get_operation_by_name("accuracy/predictions").outputs[0]
 
-predicted_pos_ids = sess.run(predictions, {input_x: features})
+predicted_pos_ids = sess.run(predictions, feed_dict={input_x: features})
 predicted_pos = textloader.pos_ids_to_pos(predicted_pos_ids)
 
 word_pos_tuples = zip(words, predicted_pos)
